f55a6712e2ac1febc80a433d25e16d7a3710086c
raynerf1 <101900356+raynerf1@users.noreply.github.com>
2024-02-17 22:05:13 +0100

    Update subsecciones.jsx

--- Frontend/src/paginas/subsecciones.jsx
+++ Frontend/src/paginas/subsecciones.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { PintarNavbarClaro, PintarNavbarOscuro } from  '../componentes/navbar';
 import { Hero1, Hero1Boton, Hero2 } from  '../componentes/heroes';
-import TarjetasSecciones from  '../componentes/tarjetas';
 import Footer from  '../componentes/footer';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom'; // Importa useNavigate
+import '../componentes/tarjetas.css';
 
 export default function Subsecciones() {
   const [subseccionData, setSubseccionData] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const storedData = localStorage.getItem('subseccionData');
@@ -18,11 +21,27 @@ export default function Subsecciones() {
     return <div>No se encuentran datos</div>;
   }
 
+  function handleClickSubseccion(event) {
+    const valorTexto = event.currentTarget.querySelector('h3.nombreSubseccion').getAttribute('nombre');
+    //const valorTexto = h3Element.textContent.trim();
+    //console.log(valorTexto)
+    
+    axios.post('http://localhost:8081/productosSQL', { subseccionNombre: valorTexto })
+      .then(response => {
+        console.log('Respuesta del servidor:', response.data);
+        localStorage.setItem('productosData', JSON.stringify(response.data));
+        navigate('/productos');
+      })
+      .catch(error => {
+        console.error('Error al enviar el valor al servidor:', error);
+      });
+  }
+
   const subsecciones = subseccionData.map(subseccion => (
     <div className="col" key={subseccion.id}>
-      <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style={{ backgroundImage: `url('${subseccion.imagen}')` }}>
+      <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style={{ backgroundImage: `url('${subseccion.imagen}')` }} onClick={handleClickSubseccion}>
         <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1 seccion">
-          <h3 className="pt-5 mt-5 mb-2 display-6 lh-1 fw-bold nombreSeccion" >
+          <h3 className="pt-5 mt-5 mb-2 display-6 lh-1 fw-bold nombreSubseccion" nombre={subseccion.subseccion}>
             {subseccion.subseccionutf != null ? subseccion.subseccionutf : subseccion.subseccion}
           </h3>
           <ul className="d-flex list-unstyled mt-auto"></ul>
@@ -36,8 +55,8 @@ export default function Subsecciones() {
       <PintarNavbarClaro
         boton1='Inicio'
         enlace1='/index'
-        boton2='Secciones'
-        enlace2='/secciones'
+        boton2=''
+        enlace2=''
         boton3=''
         enlace3=''
         boton4=''
@@ -47,8 +66,13 @@ export default function Subsecciones() {
       
       <section>
         <div className="container px-4 py-5" id="custom-cards">
-          <h1 className="pb-2 border-bottom">Subsecciones</h1>
-          <div className="row row-cols-2 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-3 row-cols-xxl-4 justify-content-start g-4 py-5">
+              <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                  <li className="breadcrumb-item navbar-item"><a href="/secciones">Secciones</a></li>
+                  <li className="breadcrumb-item item pt-2 ps-2" aria-current="page">Subsección</li>
+                </ol>
+              </nav>
+          <div className="row row-cols-2 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-3 row-cols-xxl-4 justify-content-center g-4 py-5">
             {subsecciones}
           </div>
         </div>
